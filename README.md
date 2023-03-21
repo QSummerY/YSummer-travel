b9b8f81d6a423ae6a64270b82c9fc20f5fbf6a29
YMonsterQ <jc.yang07@zuche.com>
2023-03-21 10:23:41 +0800

    add readme

---
+++ README.md
@@ -0,0 +1,7 @@
+# 出行管理
+
+## 建库
+python3 models/travel.py
+
+## 运行
+python3 app.py
--- config.py
+++ config.py
@@ -10,4 +10,3 @@ PASSWORD = 'Monster5.'
 DB_URI = 'mysql+pymysql://{}:{}@{}:{}/{}?charset=utf8'.format(USERNAME,PASSWORD,HOSTNAME,PORT,DATABASE)
 SQLALCHEMY_DATABASE_URI = DB_URI
 
-SECRET_KEY = "Monster#531."
