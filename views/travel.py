beb83e7b0460be69d700514a1b3fc8cdb7c4cec1
YMonsterQ <jc.yang07@zuche.com>
2023-03-23 13:21:55 +0800

    add update api

--- views/travel.py
+++ views/travel.py
@@ -4,35 +4,59 @@ from models.travel import Journey
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from schemas import JourneyBase
+
 engine = create_engine(DB_URI)
 Session = sessionmaker(engine)
 
 travel = APIRouter()
 
 
-
 @travel.get("/travel")
 def hello():
     return f"hello travel, {DB_URI}"
 
+
 @travel.post("/create")
 async def create(journey: JourneyBase):
     with Session() as session:
-        j1 = Journey(username = journey.username,
-                    way = journey.way,
-                    src_city = journey.src_city,
-                    src_station = journey.src_station,
-                    dest_city = journey.dest_city,
-                    dest_station = journey.dest_station,
-                    price = journey.price,
-                    departure_time = journey.departure_time,
-                    arrival_time = journey.arrival_time,
-                    desc = journey.desc,
-                    )
+        j1 = Journey(username=journey.username,
+             way=journey.way,
+             src_city=journey.src_city,
+             src_station=journey.src_station,
+             dest_city=journey.dest_city,
+             dest_station=journey.dest_station,
+             price=journey.price,
+             departure_time=journey.departure_time,
+             arrival_time=journey.arrival_time,
+             desc=journey.desc,
+             )
         session.add(j1)
         session.commit()
         session.close()
-    return {"code":200, "data": "", "msg": "Insert Success"}
+    return {"code": 200, "data": "", "msg": "Insert Success"}
+
+
+@travel.post("/update")
+async def update(journey: JourneyBase):
+    if type(journey.id) != int or journey.id == None:
+        return {"code": 400, "data": "", "msg": "field: 'id' must be Integer and not be null"}
+    with Session() as session:
+        j1 = session.query(Journey).filter(Journey.id == journey.id).first()
+        j1.username = journey.username
+        j1.way = journey.way
+        j1.src_city = journey.src_city
+        j1.src_station = journey.src_station
+        j1.dest_city = journey.dest_city
+        j1.dest_station = journey.dest_station
+        j1.price = journey.price
+        j1.departure_time = journey.departure_time
+        j1.arrival_time = journey.arrival_time
+        j1.desc = journey.desc
+
+        session.commit()
+        session.close()
+    return {"code": 200, "data": "", "msg": "Update Success"}
+
 
 @travel.get("/read_all")
 def read_all():
@@ -42,11 +66,12 @@ def read_all():
         for item in all_query:
             data.append(item.to_json())
 
-    return {"code":200, "data": data, "msg": "Query success"}
+    return {"code": 200, "data": data, "msg": "Query success"}
+
 
 @travel.get("/read_one")
 def read_one():
     with Session() as session:
         data = session.query(Journey).first().to_json()
 
-    return {"code":200, "data": data, "msg": "Query success"}
+    return {"code": 200, "data": data, "msg": "Query success"}
